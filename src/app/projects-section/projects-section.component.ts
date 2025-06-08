8bb04210c1a65f3e339d10e6cf93810cf687f365
alpay66 <allpay_66_@hotmail.de>
2025-06-08 17:41:24 +0200

    fixes

--- src/app/projects-section/projects-section.component.ts
+++ src/app/projects-section/projects-section.component.ts
@@ -1,11 +1,22 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderBarComponent } from "../hero-section/header-bar/header-bar.component";
 
 @Component({
   selector: 'app-projects-section',
-  imports: [],
+  imports: [NgIf, HeaderBarComponent],
   templateUrl: './projects-section.component.html',
   styleUrl: './projects-section.component.scss'
 })
 export class ProjectsSectionComponent {
+  showOverlay = false;
+
+  openOverlay() {
+    this.showOverlay = true;
+  }
+
+  closeOverlay() {
+    this.showOverlay = false;
+  }
 
 }
