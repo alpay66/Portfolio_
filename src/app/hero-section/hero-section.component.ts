0086f3784f5c8ebabb4812dd53124e09c9dbf08c
alpay66 <allpay_66_@hotmail.de>
2025-07-14 16:58:27 +0200

    position absolut removed hero alpay img

--- src/app/hero-section/hero-section.component.ts
+++ src/app/hero-section/hero-section.component.ts
@@ -13,6 +13,14 @@ export class HeroSectionComponent {
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+
+    if (this.menuOpen) {
+      document.body.classList.add('no-scroll');
+      document.documentElement.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+      document.documentElement.classList.remove('no-scroll');
+    }
   }
 
   @HostListener('window:resize', ['$event'])
