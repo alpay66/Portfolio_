bf675b7660843def1f4564be4aa0ff1fc12130a6
alpay66 <allpay_66_@hotmail.de>
2025-08-02 17:30:18 +0200

    project overlay edited

--- src/app/hero-section/header-bar/header-bar.component.ts
+++ src/app/hero-section/header-bar/header-bar.component.ts
@@ -1,37 +1,3 @@
-/* import { Component } from '@angular/core';
-
-type Lang = 'de' | 'en';
-
-@Component({
-  selector: 'app-header-bar',
-  standalone: true,
-  imports: [],
-  templateUrl: './header-bar.component.html',
-  styleUrls: ['./header-bar.component.scss']
-})
-export class HeaderBarComponent {
-  texts: Record<Lang, { about: string; skills: string; projects: string; contact: string }> = {
-    en: {
-      about: 'About me',
-      skills: 'Skills',
-      projects: 'Projects',
-      contact: 'Contact'
-    },
-    de: {
-      about: 'Über mich',
-      skills: 'Fähigkeiten',
-      projects: 'Projekte',
-      contact: 'Kontakt'
-    }
-  };
-
-  currentLang: Lang = 'de';
-
-  switchLanguage() {
-    this.currentLang = this.currentLang === 'de' ? 'en' : 'de';
-  }
-} */
-
 import { Component, computed, inject } from '@angular/core';
 import { LanguageService } from '../../services/language.service'; // Pfad ggf. anpassen
 
